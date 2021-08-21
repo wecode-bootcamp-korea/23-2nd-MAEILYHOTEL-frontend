8eb8e89439788d97226ce221c731d3820c6a6341
moonjuyoung1 <77542039+moonjuyoung1@users.noreply.github.com>
2021-08-22 00:13:28 +0900

    [Modify]: Navmodal 기능

--- src/component/Nav/IconsWrapper.js
+++ src/component/Nav/IconsWrapper.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import styled from 'styled-components';
+import { NavModal } from './NavModal';
 
 export const IconsWrapper = ({ LinkUrl }) => {
+  const [modal, setmodal] = useState(false);
+  const handleModal = () => {
+    setmodal(!modal);
+  };
   return (
     <StyledLink to={LinkUrl}>
-      <i className="fas fa-list-ul" />
+      <i className="fas fa-list-ul" onClick={handleModal} />
+      {modal && <NavModal handleModal={handleModal} />}
     </StyledLink>
   );
 };
